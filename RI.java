6c2045797816b7ebfb563a8cfab7c77ee38a166c
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-03-31 21:10:13 +0530

    leetcode problem 7 solution added

---
+++ RI.java
@@ -0,0 +1,26 @@
+public class RI {
+  public static void main(String[] args) {
+    System.out.println(reverseInteger(-123));
+  }
+
+  static int reverseInteger(int n) {
+    double result = 0;
+    boolean isNegative = n < 0;
+    n = n < 0 ? n * -1 : n;
+
+    while (n != 0) {
+      result = result * 10 + n % 10;
+      n /= 10;
+    }
+
+    if (isNegative) {
+      result *= -1;
+    }
+
+    if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) {
+      return 0;
+    }
+
+    return (int) result;
+  }
+}
