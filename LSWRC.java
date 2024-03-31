c685c49156d3e02bf40e82a1d7b4b979046cf3d5
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-03-31 20:47:00 +0530

    leetcode problem 3 solution added

---
+++ LSWRC.java
@@ -0,0 +1,20 @@
+public class LSWRC {
+  public static void main(String[] args) {
+    String s = "abcabcbb";
+
+    System.out.println(lengthOfLongestSubstring(s));
+  }
+
+  static int lengthOfLongestSubstring(String s) {
+    int result = 0, left = 0;
+    int[] index = new int[128];
+
+    for (int i = 0; i < s.length(); i++) {
+      left = Math.max(left, index[s.charAt(i)]);
+      index[s.charAt(i)] = i + 1;
+      result = Math.max(result, i - left + 1);
+    }
+
+    return result;
+  }
+}
