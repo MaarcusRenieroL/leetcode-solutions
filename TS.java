8f57c94de36da63377db4ba9896cc898098ac41c
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-03-31 20:07:29 +0530

    leetcode problem 1 solution added

---
+++ TS.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class TS {
+  public static void main(String[] args) {
+    int[] nums = { 2, 7, 11, 15 };
+    int target = 9;
+
+    int[] result = twoSum(nums, target);
+
+    System.out.println("[" + result[0] + ", " + result[1] + "]");
+  }
+
+  static int[] twoSum(int[] nums, int target) {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    int[] result = new int[2];
+
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(target - nums[i])) {
+        result[0] = map.get(target - nums[i]);
+        result[1] = i;
+        break;
+      } else {
+        map.put(nums[i], i);
+      }
+    }
+
+    return result;
+  }
+}
