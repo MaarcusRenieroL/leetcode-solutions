74e180ed406b7e71ca9ef9bdb73fc9a857f66b92
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-04-02 23:03:48 +0530

    leetcode problem 34 solution added

---
+++ 34.java
@@ -0,0 +1,39 @@
+class Solution {
+  public int[] searchRange(int[] nums, int target) {
+    int[] res = new int[] { -1, -1 };
+    int l = 0, r = nums.length - 1, index = -1;
+
+    while (l <= r) {
+      int m = l + (r - l) / 2;
+
+      if (arr[m] == target) {
+        index = m;
+        break;
+      } else if (arr[m] > target) {
+        r = m - 1;
+      } else {
+        l = m + 1;
+      }
+    }
+
+    if (index == -1) {
+      return res;
+    } else {
+      l = index;
+      r = index;
+
+      while (l >= 0 && nums[l] == target) {
+        l--;
+      }
+
+      while (r < nums.length && nums[r] == target) {
+        r++;
+      }
+    }
+
+    res[0] = l + 1;
+    res[1] = r - 1;
+
+    return res;
+  }
+}
