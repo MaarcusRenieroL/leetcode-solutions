dd8f4e7bd54443db040d678a7d311e940c3b89cc
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-04-05 06:36:01 +0530

    leetcode problem 1544 solution added

---
+++ 1544.java
@@ -0,0 +1,22 @@
+class Solution {
+  public String makeGood(String str) {
+    Stack<Character> s = new Stack<>();
+
+    for (char c : str.toCharArray()) {
+      if (!s.isEmpty() && Math.abs(c - s.peek()) == 32) {
+        s.pop();
+      } else {
+        s.push(c);
+      }
+    }
+
+    StringBuilder r = new StringBuilder();
+
+    for (int i = s.size() - 1; i >= 0; i--) {
+      r.insert(0, s.pop());
+    }
+
+    return r.toString();
+
+  }
+}
