fd5709bc98c5a732e601042cdf0789dce1602500
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-04-11 23:01:04 +0530

    leetcode problem 402 solution added

---
+++ 402.java
@@ -0,0 +1,40 @@
+class Solution {
+  public String removeKdigits(String num, int k) {
+    int l = num.length(), c = 0;
+    Stack<Character> s = new Stack<>();
+    StringBuilder sb = new StringBuilder();
+
+    if (k == l) {
+      return "0";
+    }
+
+    while (c < l) {
+      while (k > 0 && !s.isEmpty() && s.peek() > num.charAt(c)) {
+        s.pop();
+        k--;
+      }
+
+      s.push(num.charAt(c));
+      c++;
+    }
+
+    while (k > 0) {
+      s.pop();
+      k--;
+    }
+
+    while (!s.isEmpty()) {
+      char cc = s.pop();
+      sb.append(cc);
+    }
+
+    sb.reverse();
+
+    while (sb.length() > 1 && sb.charAt(0) == '0') {
+      sb.deleteCharAt(0);
+    }
+
+    return sb.toString();
+
+  }
+}
